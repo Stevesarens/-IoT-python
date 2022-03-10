69749a9e6656a695e7d6058c4218c0472dd5b0a5
Steve Sarens <steve.sarens@ap.be>
2022-03-10 21:14:47 +0100

    bijvoegen file

---
+++ Labo 03/hello_world.py
@@ -0,0 +1,17 @@
+greeting ='hello'
+planet = 'world'
+greeting + '' + planet
+print("hello")
+print(planet)
+print(12 * 18)
+result = 112 / 14
+print('The result is', result)
+print('I\'m OK')
+print('''I've got this!''')
+print('this'
+      'string is'
+      'split over'
+      'multiple lines')
+
+hexstr = hex(12648430)
+print(hex(3735928559))
