e1d473ce9465f356220a13899c4cdc519a637b91
Steve Sarens <steve.sarens@ap.be>
2022-03-14 19:02:49 +0100

    Oefenign toegevoegd

---
+++ Labo 04/optelling.py
@@ -0,0 +1,8 @@
+print('de som van')
+getal1 = input ('getal 1')
+print('getal 1:', getal1)
+print('en')
+getal2 = input ('getal 2 ') 
+print('getal 2:', getal2)
+som = int(getal1)  + int(getal2)
+print('is:',som)
