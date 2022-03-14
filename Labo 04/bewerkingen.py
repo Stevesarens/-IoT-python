c08703b619fe73888cd208d92a1eaf5831977f99
Steve Sarens <steve.sarens@ap.be>
2022-03-14 19:52:26 +0100

    oefening toegevoegd

---
+++ Labo 04/bewerkingen.py
@@ -0,0 +1,11 @@
+bewerking = input ('Bewerking')
+if bewerking == '+':
+    print('Optellen')
+elif bewerking == '-':
+    print('aftrekken')
+elif bewerking == 'x':
+    print ('vermenigvuldigen')
+elif bewerking == '/':
+    print('delen')
+else:
+    print('Bewerking is niet ge\xefmplementeerd')
--- Labo 04/optelling.py
+++ Labo 04/optelling.py
@@ -6,3 +6,7 @@ getal2 = input ('getal 2 ')
 print('getal 2:', getal2)
 som = int(getal1)  + int(getal2)
 print('is:',som)
+if som == int(5):
+    print('positief')
+elif som == '-':
+    print('negatief')
